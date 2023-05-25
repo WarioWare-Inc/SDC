fc83fb48cba3859b3c552fe0fa9b032812b86c15
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-05-24 21:27:59 -0700

    Completed schema.sql and ETL

---
+++ db-schemas/overview/schema.sql
@@ -0,0 +1,52 @@
+\c productDB
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id SERIAL UNIQUE PRIMARY KEY,
+  product_name TEXT,
+  slogan TEXT,
+  prod_description TEXT,
+  category TEXT,
+  default_price VARCHAR(15)
+);
+
+DROP TABLE IF EXISTS styles;
+CREATE TABLE IF NOT EXISTS styles (
+  id SERIAL UNIQUE PRIMARY KEY,
+  productId INTEGER REFERENCES products(id),
+  style_name VARCHAR(200),
+  sale_price VARCHAR(15),
+  original_price VARCHAR(15),
+  default_style SMALLINT
+);
+
+DROP TABLE IF EXISTS photos;
+CREATE TABLE IF NOT EXISTS photos (
+  id SERIAL UNIQUE PRIMARY KEY,
+  styleId INTEGER REFERENCES styles(id),
+  photo_url TEXT,
+  thumbnail_url TEXT
+);
+
+DROP TABLE IF EXISTS related; -- need to fix
+CREATE TABLE IF NOT EXISTS related (
+  id SERIAL UNIQUE PRIMARY KEY,
+  current_product_id INTEGER REFERENCES products(id),
+  related_product_id INTEGER
+);
+
+DROP TABLE IF EXISTS features;
+CREATE TABLE IF NOT EXISTS features (
+  id SERIAL UNIQUE PRIMARY KEY,
+  product_id INTEGER REFERENCES products(id),
+  feature VARCHAR(200),
+  val VARCHAR(200) NULL
+);
+
+DROP TABLE IF EXISTS skus;
+CREATE TABLE IF NOT EXISTS skus (
+  id SERIAL UNIQUE PRIMARY KEY,
+  styleId INTEGER REFERENCES styles(id),
+  size VARCHAR(10),
+  quantity SMALLINT
+);
