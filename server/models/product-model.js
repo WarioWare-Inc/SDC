7db0e8fce23d0f0cda6014a344a96947a6b31015
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-05-26 09:43:05 -0700

    Completed basic queries for API

--- server/models/product-model.js
+++ server/models/product-model.js
@@ -11,11 +11,23 @@ const {
 } = require('./example-data');
 
 module.exports = {
-  getProductsDB(callback) {
-    callback(null, testProducts);
+  getProductsDB(callback, limit = 5) {
+    client.query(`SELECT
+    products.id,
+    products.product_name AS "name",
+    products.slogan,
+    products.prod_description AS "description",
+    products.category,
+    products.default_price
+    FROM products LIMIT = $1`, [limit], (err, result) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, result);
+      }
+    });
   },
   getProductDB(productID, callback) {
-    // callback(null, testProduct);
     client.query('SELECT products.*, features.feature, features.val FROM products JOIN features ON products.id = features.product_id WHERE products.id = $1', [productID], (err, result) => {
       if (err) {
         console.log('there was an error', err);
@@ -42,11 +54,9 @@ module.exports = {
     });
   },
   getStylesDB(productID, callback) {
-    // callback(null, testStyles); // skus, styles, and photos
     client.query('SELECT styles.id AS style_id, styles.style_name AS "name", styles.original_price, styles.sale_price, styles.default_style AS "default?", (SELECT json_agg(nested_photos) FROM (SELECT photos.thumbnail_url, photos.photo_url AS url FROM photos WHERE photos.styleid = styles.id) AS nested_photos) AS photos, (SELECT json_object_agg(nested_skus.id, nested_skus) FROM (SELECT skus.id, skus.quantity, skus.size FROM skus WHERE skus.styleid = styles.id) AS nested_skus) AS skus FROM styles WHERE styles.productid = $1', [productID], (err, result) => {
       if (err) {
-        console.log('error occurred', err);
-        callback('err is', err);
+        callback(err);
       } else {
         const resultsObject = {
           product_id: productID,
@@ -57,7 +67,6 @@ module.exports = {
     });
   },
   getRelatedDB(productID, callback) {
-    // callback(null, testRelated);
     client.query('SELECT array_agg(related.related_product_id) FROM products JOIN related ON products.id = related.current_product_id WHERE products.id = $1', [productID], (err, result) => {
       if (err) {
         callback(err);
