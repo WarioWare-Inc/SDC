2e3d3fea4f8218d6fa42f0be6fadc5f3f5c13bfa
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-05-29 13:37:22 -0700

    Finished stress testing through k6

--- server/models/k6script.js
+++ server/models/k6script.js
@@ -2,16 +2,46 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  vus: 10,
-  duration: '30s',
+  stages: [
+    { duration: '30s', target: 10 },
+    { duration: '30s', target: 20 },
+    { duration: '30s', target: 30 },
+    { duration: '30s', target: 40 },
+    { duration: '30s', target: 30 },
+    { duration: '30s', target: 20 },
+    { duration: '30s', target: 10 },
+  ],
 };
 
-export default function () {
-  http.get('http://localhost:3000/products/40344');
-  sleep(1);
-  // const res = http.get('localhost:3000/products/40344/');
-  // check(res, {
-  //   'is status 200': (r) => r.status === 200,
-  // });
-  // sleep(1);
-}
+// // for testing /products endpoint:
+// export default function () {
+//   const res = http.get('http://localhost:3000/products/40344');
+//   check(res, { 'status was 200': (r) => r.status === 200 });
+//   sleep(1);
+// }
+
+// // for testing /products/:product_id endpoint:
+// export default function () {
+//   for (let i = 900000; i < 920000; i++) {
+//     const res = http.get(`http://localhost:3000/products/${i}`);
+//     check(res, { 'status was 200': (r) => r.status === 200 });
+//     sleep(1);
+//   }
+// }
+
+// for testing /products/:product_id/styles endpoint:
+// export default function () {
+//   for (let i = 920001; i < 940000; i++) {
+//     const res = http.get(`http://localhost:3000/products/${i}/styles`);
+//     check(res, { 'status was 200': (r) => r.status === 200 });
+//     sleep(1);
+//   }
+// }
+
+// export default function () {
+//   for (let i = 940001; i < 960000; i++) {
+//     const res = http.get(`http://localhost:3000/products/${i}/related`);
+//     check(res, { 'status was 200': (r) => r.status === 200 });
+//     sleep(1);
+//   }
+// }
